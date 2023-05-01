ce9eafa7a04d3dc1c616be07cb6129aa65885110
Talha Rajpoot <rt3830279@gmail.com>
2023-05-01 13:00:36 +0500

    data type

--- Data type/script.js
+++ Data type/script.js
@@ -4,5 +4,9 @@
 //  There Are following data types
 
 // 1 : var a = ("Java script");   -------------------->  string
-// 2 : var a = (56);              -------------------->   Numbers
-// 3 : var a = (true , false);    -------------------->   Bu
+// 2 : var a = (56);              -------------------->  Numbers
+// 3 : var a = (true , false);    -------------------->  Boolean
+// 4 : var a = ["Talha", "Awais"];-------------------->  Arry 
+// 5 : var a = {"Talha"};         -------------------->  Object
+// 6 : var a ;                    -------------------->  Undefine
+// 7 : var a = (null);            -------------------->  Null
