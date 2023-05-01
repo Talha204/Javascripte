5df1c75f163393daac40d658df7a0390070d3bf1
Talha Rajpoot <rt3830279@gmail.com>
2023-05-01 11:57:23 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// 
+
+//                                  variables
+//  There are three types of variable these are following 
+//  1 : var  
+//  2 : let 
+//  3 : constant
+
+
+//    Frist type of variable is var 
+
+var a = ("This is a frist type of variable")
+
+console.log("a")
+
+
+// 
