463132c80cc05bb844733d188c93417fca12abec
Talha Rajpoot <rt3830279@gmail.com>
2023-05-01 12:28:21 +0500

    second

--- script.js
+++ script.js
@@ -7,11 +7,33 @@
 //  3 : constant
 
 
-//    Frist type of variable is var 
+//                             Frist type of variable is var 
 
-var a = ("This is a frist type of variable")
+// var a = ("Talha");
+// a = ("Awais")
+// var a = ("awaia talha")
 
-console.log("a")
+// console.log("first" , a)
 
 
-// 
+//                             second type of variable is let 
+
+// let b = ("talha");
+// b = ("Awais");
+// let b = ("awais talha");
+
+
+// console.log("second" , b);
+
+//    let cannot again declared 
+
+//                            Third type of variable is constant 
+
+// const c = ("talha")
+// c = ("awais ")
+// const c = ("Talha awais")
+
+
+// console.log("third :" , c);
+
+//   constant variable value cannot bee change 
