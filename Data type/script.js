029a0dfa9432087a101290c6b46f7b829654fde2
Talha Rajpoot <rt3830279@gmail.com>
2023-05-01 12:48:38 +0500

    data type

---
+++ Data type/script.js
@@ -0,0 +1,8 @@
+//   
+
+
+//  There Are following data types
+
+// 1 : var a = ("Java script");   -------------------->  string
+// 2 : var a = (56);              -------------------->   Numbers
+// 3 : var a = (true , false);    -------------------->   Bu
