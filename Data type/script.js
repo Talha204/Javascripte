0ac4095369c3b14b2264f0214683006d9743a3b4
Talha Rajpoot <rt3830279@gmail.com>
2023-05-01 13:38:16 +0500

    data type

--- Data type/script.js
+++ Data type/script.js
@@ -10,3 +10,63 @@
 // 5 : var a = {"Talha"};         -------------------->  Object
 // 6 : var a ;                    -------------------->  Undefine
 // 7 : var a = (null);            -------------------->  Null
+
+//                    This is a frist type of data is "string"
+
+// var a = ("Talha")
+// console.log(a);
+
+// console.log( typeof a)
+
+
+
+//                    This is a 2 type of data is Number
+
+
+// var a = (92);
+// console.log(a);
+
+// console.log( typeof a);
+
+
+
+//                    This is a 3 type of data is boolean
+
+
+// var a = (true);
+// console.log(a);
+
+// console.log( typeof a);
+
+
+//                    This is a 4 type of data is ["Arry"]
+
+
+// var a = ["Java" , "script"];
+// console.log(a);
+
+// console.log( typeof a);
+
+//                    This is a 5 type of data is {"Object"}
+
+
+// var a = {first :"Java" , last :"script"};
+// console.log(a);
+
+// console.log( typeof a);
+
+
+//                   This is a 6 type of data is undefine
+
+
+// var a;
+// console.log(a);
+
+// console.log( typeof a);
+
+//                   This is a 7 type of data is null
+
+// var a = (null);
+// console.log(a);
+
+// console.log( typeof a);
