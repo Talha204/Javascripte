5f6a245caf522d801cb117f9c41f645a8e3080cf
Talha Rajpoot <rt3830279@gmail.com>
2023-05-02 10:31:54 +0500

    operator

--- 3 Arithmatic operations/script.js
+++ 3 Arithmatic operations/script.js
@@ -29,3 +29,55 @@
 //  var c = (a - b);
 
 // console.log(c);
+
+
+//                            multiplication
+
+//  var a = (200);
+//  var b = (34);
+//  var c = (a * b);
+
+// console.log(c);
+
+
+//                              division
+
+//  var a = (14);
+//  var b = (2);
+//  var c = (a / b);
+
+// console.log(c);
+
+
+//                            Exponentiation
+
+//  var a = (2);
+//  var b = (10);
+//  var c = (a ** b);
+
+// console.log(c);
+
+
+//                             modulus(Riminder)
+
+//  var a = (10);
+//  var b = (2);
+//  var c = (a % b);
+
+// console.log(c);
+
+
+//                              increment
+
+//  var a = (10);
+//  var b = (2);
+//  a++
+// console.log(a + b);
+
+
+//                              decrement
+
+//  var a = (87);
+//  var b = (64);
+//  a--
+// console.log(a - b);
