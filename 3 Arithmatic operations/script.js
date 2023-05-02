057fd8d681bfb25571898201d247eed3a717600c
Talha Rajpoot <rt3830279@gmail.com>
2023-05-02 10:16:09 +0500

    opeartor

--- 3 Arithmatic operations/script.js
+++ 3 Arithmatic operations/script.js
@@ -11,3 +11,21 @@
 //   ++           =        increment
 //   --           =        decrement 
 
+
+
+
+//                             Addition
+
+//  var a = (12);
+//  var b = (19);
+//  var c = (a + b);
+
+// console.log(c);
+
+//                             subtraction
+
+//  var a = (10);
+//  var b = (2);
+//  var c = (a - b);
+
+// console.log(c);
