fac327686b0dbc7a12adbcadd4e81183b8748734
Talha Rajpoot <rt3830279@gmail.com>
2023-05-03 13:00:42 +0500

    .

--- 5 Comparsion Opeartors/script.js
+++ 5 Comparsion Opeartors/script.js
@@ -21,13 +21,53 @@
 // var b = (12)
 
 // console.log(a == b);
-
+//                     equal to 
 
 
 // var a = ("12")
 // var b = ("12")
 
 // console.log(a === b);
+//                  equal value and equal type   
+
+// var a = (9)
+// var b = (10)
+
+// console.log(a != b);
+//                       Not equal       
+
+
+// var a = (9);
+// var b = ("10");
+
+// console.log(a !== b);
+//                       not equal value and not equal type 
+
+
+// var a = (12);
+// var b = (10);
+
+// console.log(a > b);
+//                         greater than
+
+
+// var a = (5);
+// var b = (10);
+
+// console.log(a < b);
+//                        less than 
+
+
+// var a = (10);
+// var b = (10);
+
+// console.log(a <= b);
+//                       greater than and equal to
 
 
 
+// var a = (10);
+// var b = (10);
+
+// console.log(a >= b);
+//                       less than and equal to
