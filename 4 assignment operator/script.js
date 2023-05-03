bb2db9871fcb721fee8f6ec74dfba90b3edf191d
Talha Rajpoot <rt3830279@gmail.com>
2023-05-03 10:53:29 +0500

    assi

---
+++ 4 assignment operator/script.js
@@ -0,0 +1,11 @@
+//
+
+// Operators             ||          Example          ||        Same as  
+
+//  =                    ||            x=y            ||          x=y 
+//  +=                   ||            x+=y           ||         x=x+y 
+//  -=                   ||            x-=y           ||         x=x-y
+//  *=                   ||            x*=y           ||         x=x*y
+//  /=                   ||            x/=y           ||         x=x/y
+//  %=                   ||            x%=y           ||         x=x%y
+//  **=                  ||            x**=y          ||         x=x**y
