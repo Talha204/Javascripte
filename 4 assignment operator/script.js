7fa755cb75fbc7ff605e12fe7a5dc6a1ccb3b05c
Talha Rajpoot <rt3830279@gmail.com>
2023-05-03 12:14:25 +0500

    confirm

--- 4 assignment operator/script.js
+++ 4 assignment operator/script.js
@@ -9,3 +9,12 @@
 //  /=                   ||            x/=y           ||         x=x/y
 //  %=                   ||            x%=y           ||         x=x%y
 //  **=                  ||            x**=y          ||         x=x**y
+
+//                     Example
+
+// var a = (12);
+// var b = (10);
+
+// a /= b;
+
+// console.log(a);
