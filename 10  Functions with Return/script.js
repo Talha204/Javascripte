1a4c23c52e9847dc62d7d4014ca00034d4e61dd4
Talha Rajpoot <rt3830279@gmail.com>
2023-05-04 12:35:28 +0500

    .

--- 10  Functions with Return/script.js
+++ 10  Functions with Return/script.js
@@ -1,23 +1,39 @@
 //
 
 
-function sufi(num1 , num2) {
-    var a = (num1 + num2);
+// function sufi(num1 , num2) {
+//     var a = (num1 + num2);
+
+//     return a;
+// }
+
+
+// var total = sufi(10 , 70);
+// per(total)
+
+
+// function per (total){
+//     var b = total/200 * 100
+//     console.log(b);
+// }
+
+
+
+
+function subject(math , eng , urdu , ist) {
+    var a = (math + eng + urdu + ist);
 
     return a;
 }
 
 
-var total = sufi(10 , 70);
-per(total)
-
-
-function per (total){
-    var b = total/200 * 100
-    console.log(b);
+function percentage(sub){
+    var per = sub/400 * 100
+    console.log(per "%");
 }
 
-
+var sub = subject(50 , 50 , 50 , 50);
+percentage(sub)
 
 
 
