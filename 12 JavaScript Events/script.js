4b41c1b52299492520fd1e91c97f83a6750981c4
Talha Rajpoot <rt3830279@gmail.com>
2023-05-05 15:53:45 +0500

    .

--- 12 JavaScript Events/script.js
+++ 12 JavaScript Events/script.js
@@ -1,9 +1,10 @@
 //
 
-var heading = document.getElementById("heading")
+var heading = document.getElementById("heading");
 
 function naveed() {
-    heading.innerHTML = "This is new heading"
+    heading.innerText = "This is a new heading"
 }
 
 
+// naveed();
