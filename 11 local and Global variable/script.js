7db1edfbd406f9211a1cdcbff2f5dd8da285629f
Talha Rajpoot <rt3830279@gmail.com>
2023-05-05 14:57:22 +0500

    .

---
+++ 11 local and Global variable/script.js
@@ -0,0 +1,20 @@
+//
+
+
+//      This is a global variable 
+
+
+var a = ("talha");
+
+function awais() {
+    var a = (12+12)
+    console.log(a);
+}
+
+
+
+console.log(a);
+awais();
+awais();
+awais();
+awais();
