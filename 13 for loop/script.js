a7570507847d6958b37c984dd394daeea50dd066
Talha Rajpoot <rt3830279@gmail.com>
2023-05-06 13:18:56 +0500

    .

--- 13 for loop/script.js
+++ 13 for loop/script.js
@@ -1,6 +1,12 @@
 //
 
-for (let talha = 0; talha <= 100; talha++) {
+for (let talha = 0; talha <= 10; talha++) {
 
     console.log("talha is a boy")
+}
+
+
+for (let index = 1; index <= 50; index++) {
+
+    document.write("Talha is very best develper");
 }
