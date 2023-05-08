9802c762e73f393518e15a4449d2dc911d42e0be
Talha Rajpoot <rt3830279@gmail.com>
2023-05-08 13:25:28 +0500

    .

--- 13 for loop/script.js
+++ 13 for loop/script.js
@@ -1,7 +1,14 @@
 //
 
+
+
+
 for (var talha = 0; talha <= 10; talha++) {
 
     console.log(talha , "talha");
 }
 
+
+for (var awais = 20; awais >= 2; awais--) {
+    console.log(awais , "awais");
+}
