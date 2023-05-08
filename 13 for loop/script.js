4363b244782b4b103631f4641dfcfd96bcf59d85
Talha Rajpoot <rt3830279@gmail.com>
2023-05-08 13:33:22 +0500

    .

--- 13 for loop/script.js
+++ 13 for loop/script.js
@@ -3,12 +3,16 @@
 
 
 
-for (var talha = 0; talha <= 10; talha++) {
+// for (var talha = 0; talha <= 10; talha++) {
 
-    console.log(talha , "talha");
-}
+//     console.log(talha , "talha");
+// }
 
 
-for (var awais = 20; awais >= 2; awais--) {
-    console.log(awais , "awais");
+// for (var awais = 20; awais >= 2; awais--) {
+//     console.log(awais , "awais");
+// }
+
+for (var saeed = 100;  saeed >= 10;  saeed--){
+    console.log("naveed is best" , saeed);
 }
